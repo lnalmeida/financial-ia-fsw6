b3edfd5b4eb6b2163043eb8e6e21feca80fe75aa
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-11-07 18:09:07 -0300

    chore: adds shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Home Page</h1>
+    <div className="flex w-screen items-center justify-center py-5">
+      <Button> Home Page</Button>
     </div>
   );
 }
