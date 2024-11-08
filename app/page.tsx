dbd31f86238a6bc4ace020c211f74b1fbc39ac96
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-11-07 23:19:24 -0300

    chore: add lint-staged and husky libs

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,13 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const name = "";
-
   const { userId } = await auth();
 
   if (!userId) {
     redirect("/login");
   }
   return (
-         <div className="flex h-full w-screen items-center justify-center py-5">
+    <div className="flex h-full w-screen items-center justify-center py-5">
       <UserButton showName />
     </div>
   );
