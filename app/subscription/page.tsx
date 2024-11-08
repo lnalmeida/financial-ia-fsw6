e2a657c55aa6af65992bb85a5f5b43049b61c423
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-11-07 22:27:25 -0300

    feat: add Login page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,9 @@
-const Subscription = () => {
-    return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1>Subscription Page</h1>
+const SubscriptionPage = () => {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <h1>SubscriptionPage Page</h1>
     </div>
-    )
-}
+  );
+};
 
-export default Subscription;
+export default SubscriptionPage;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,9 @@
-const Transactions = () => {
-    return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1>Transactions Page</h1>
+const TransactionsPage = () => {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <h1>Transactions Page</h1>
     </div>
-    )
-}
+  );
+};
 
-export default Transactions;
+export default TransactionsPage;
